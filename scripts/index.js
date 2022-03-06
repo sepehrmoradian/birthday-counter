2efe094672057b5dc05852a1ebe4249dff902d60
Sepehr <sepehrmoradian18@gmail.com>
2022-03-06 18:13:51 +0100

    update

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'March 7, 2022',
+  name: 'Fatemeh'
 };
 
 function hideEverything() {
